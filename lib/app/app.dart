b25b47a6c48a42c714e07823b6d924623a3f66a2
AmmarAgeeza <ammarfathy516@gmail.com>
2024-01-04 00:20:34 +0200

    inital commit

---
+++ lib/app/app.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class LokMartApp extends StatelessWidget {
+  const LokMartApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Flutter Demo',
+      home: Scaffold(),
+    );
+  }
+}
